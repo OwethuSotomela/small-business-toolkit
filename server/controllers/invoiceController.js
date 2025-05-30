eefe2db9f8d274069d766ee6029ec9699fb6eaa1
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:30:07 +0200

    Update invoiceController.js

--- server/controllers/invoiceController.js
+++ server/controllers/invoiceController.js
@@ -1,1 +1,20 @@
+const Invoice = require('../models/Invoice');
 
+exports.getInvoices = async (req, res) => {
+  try {
+    const invoices = await Invoice.find();
+    res.status(200).json(invoices);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error });
+  }
+};
+
+exports.createInvoice = async (req, res) => {
+  try {
+    const newInvoice = new Invoice(req.body);
+    await newInvoice.save();
+    res.status(201).json(newInvoice);
+  } catch (error) {
+    res.status(400).json({ message: 'Error creating invoice', error });
+  }
+};
