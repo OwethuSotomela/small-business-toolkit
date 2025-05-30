a3b0f82932fd6fd9a297be9462fd08d2570d11ad
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:31:41 +0200

    Update salesController.js

--- server/controllers/salesController.js
+++ server/controllers/salesController.js
@@ -1,1 +1,13 @@
+const Transaction = require('../models/Transaction');
 
+// Get sales summary (basic analytics)
+exports.getSalesSummary = async (req, res) => {
+  try {
+    const totalSales = await Transaction.aggregate([
+      { $group: { _id: null, total: { $sum: "$amount" } } }
+    ]);
+    res.status(200).json({ total: totalSales[0]?.total || 0 });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching sales summary', error });
+  }
+};
