07a287e5ea92c8d166c6c8d016aaa47438eeaa4c
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:42:54 +0200

    Update salesRoutes.js

--- server/routes/salesRoutes.js
+++ server/routes/salesRoutes.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const salesController = require('../controllers/salesController');
 
+router.get('/summary', salesController.getSalesSummary);
+
+module.exports = router;
