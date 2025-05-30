733acd6bcea78e0cd71a1444c2091226d70a2e5d
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:34:31 +0200

    Update Invoice.js

--- server/models/Invoice.js
+++ server/models/Invoice.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const invoiceSchema = new mongoose.Schema({
+  customer: String,
+  items: Array,
+  total: Number,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Invoice', invoiceSchema);
