13ad05b168a500075dd05be345fb8793ae5b9225
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-31 00:10:53 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Inventory from './pages/business/Inventory';
+import Transactions from './pages/business/Transactions';
+import Invoices from './pages/business/Invoices';
+import SalesAnalytics from './pages/business/SalesAnalytics';
+import MarketingTips from './pages/business/MarketingTips';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import Dashboard from './pages/Dashboard';
-import PrivateRoute from './components/PrivateRoute';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        } />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/sales-analytics" element={<SalesAnalytics />} />
+        <Route path="/marketing-tips" element={<MarketingTips />} />
       </Routes>
     </Router>
   );
