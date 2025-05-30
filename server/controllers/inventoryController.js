6ad4cbff5c6d29c4bc4b1680d8609cc7715b97b8
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:29:43 +0200

    Update inventoryController.js

--- server/controllers/inventoryController.js
+++ server/controllers/inventoryController.js
@@ -1,1 +1,22 @@
+const Inventory = require('../models/Inventory');
 
+// Get all inventory items
+exports.getInventory = async (req, res) => {
+  try {
+    const items = await Inventory.find();
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error });
+  }
+};
+
+// Add new inventory item
+exports.addInventory = async (req, res) => {
+  try {
+    const newItem = new Inventory(req.body);
+    await newItem.save();
+    res.status(201).json(newItem);
+  } catch (error) {
+    res.status(400).json({ message: 'Error adding item', error });
+  }
+};
