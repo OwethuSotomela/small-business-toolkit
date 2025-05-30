5a684c8b84db2dd090fb016816ade6190390b2eb
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:42:09 +0200

    Update transactionRoutes.js

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const transactionController = require('../controllers/transactionController');
 
+router.get('/', transactionController.getTransactions);
+router.post('/', transactionController.addTransaction);
+
+module.exports = router;
