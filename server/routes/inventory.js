ff2977940f6f3be90f93cf26f1cf5b1ddf2ce1f4
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:23:36 +0200

    Update inventory.js

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,1 +1,25 @@
+const router = require('express').Router();
+const Inventory = require('../models/Inventory');
 
+// Add item
+router.post('/', async (req, res) => {
+  try {
+    const newItem = new Inventory(req.body);
+    const saved = await newItem.save();
+    res.json(saved);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get all items
+router.get('/', async (req, res) => {
+  try {
+    const items = await Inventory.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
