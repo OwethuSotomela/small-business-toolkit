26285d3e566df01da80df18424d931a543dc1b6d
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:57:17 +0200

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,43 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET || 'secret', {
+    expiresIn: '7d'
+  });
+};
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ name, email, password });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      token: generateToken(user._id)
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && await user.matchPassword(password)) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        token: generateToken(user._id)
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
