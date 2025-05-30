10b4b5aadb798880cf14d374645e57f211328317
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:04:12 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorker';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// Register service worker for PWA
+serviceWorkerRegistration.register();
