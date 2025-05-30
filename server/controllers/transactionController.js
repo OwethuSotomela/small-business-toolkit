016b86c51f89412ed3e2d5251c5b91bd067928cd
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:30:55 +0200

    Update transactionController.js

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -1,1 +1,22 @@
+const Transaction = require('../models/Transaction');
 
+// Get all transactions
+exports.getTransactions = async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    res.status(200).json(transactions);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error });
+  }
+};
+
+// Record new transaction
+exports.addTransaction = async (req, res) => {
+  try {
+    const newTransaction = new Transaction(req.body);
+    await newTransaction.save();
+    res.status(201).json(newTransaction);
+  } catch (error) {
+    res.status(400).json({ message: 'Error saving transaction', error });
+  }
+};
