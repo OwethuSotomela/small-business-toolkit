ccee2c4168f9a092fcbcb1c88b4142f01fb965de
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:21:50 +0200

    Update Inventory.js

--- server/models/Inventory.js
+++ server/models/Inventory.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const InventorySchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+  price: Number,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Inventory', InventorySchema);
