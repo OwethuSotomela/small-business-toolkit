6258e6f0701073dcbc00f9766e17949dcc21445e
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:41:17 +0200

    Update invoiceRoutes.js

--- server/routes/invoiceRoutes.js
+++ server/routes/invoiceRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const invoiceController = require('../controllers/invoiceController');
 
+router.get('/', invoiceController.getInvoices);
+router.post('/', invoiceController.addInvoice);
+
+module.exports = router;
