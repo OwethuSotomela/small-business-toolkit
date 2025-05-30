3d5d8f9f5310959e36504ee17fe5b15ef08f9a0d
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-31 00:08:03 +0200

    Update MarketingTips.js

--- client/src/pages/business/MarketingTips.js
+++ client/src/pages/business/MarketingTips.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const MarketingTips = () => {
+  return (
+    <div>
+      <h2>📣 Marketing Tips</h2>
+      <p>Grow your business with simple, effective tips.</p>
+    </div>
+  );
+};
+
+export default MarketingTips;
