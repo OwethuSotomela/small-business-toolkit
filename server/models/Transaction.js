1cb782058a72a53708bfb485b0f202531f2b2b32
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-05-30 22:37:44 +0200

    Update Transaction.js

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const transactionSchema = new mongoose.Schema({
+  type: String, // sale or expense
+  amount: Number,
+  description: String,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
