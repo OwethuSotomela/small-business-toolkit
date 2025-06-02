0c259f43a63b65ea286e73373d7e5c36b187bbc0
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-06-02 09:14:07 +0200

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
 
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api', // or your live server URL
+});
+
+// Optional: attach token if using auth
+API.interceptors.request.use((req) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
+  }
+  return req;
+});
+
+export default API;
