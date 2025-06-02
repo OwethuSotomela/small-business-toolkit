458b1b1cdc07dedfa873b81b99e6b7ce8875e287
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-06-02 09:15:04 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorker';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
